9fa571bf9c46fb0d1e765069a037b34901ca35d5
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2024-12-30 16:50:27 +0100

    revisit current code a bit

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,3 +1,4 @@
+#define _XOPEN_SOURCE   600
 #define _POSIX_C_SOURCE 200112L
 
 #include "../include/libk.h"
@@ -6,7 +7,6 @@
 
 //void _kernel_init(int argc, char* argv[]) {
 void _kernel_init(void) { // The question is does argc argv work on bare metal ? (yes i know i need to use my own bootloader for this)
-
 
   vt_clear(VGA_COLOR);
 	printk("BismuthOS version " VERSION " (under contruction)\n", 0);
@@ -20,16 +20,22 @@ void _kernel_init(void) { // The question is does argc argv work on bare metal ?
   vt_line = 6;
   vt_col = 0;
 
+  /*
   for(int i = 0; i < 80; i++)
     printf("terminal scrolling ?\n");
+  */
 
   /*
     We do not know the fat32/tar initrd end, so we only define the begin ptr
     ... and assume every byte after exists (end or not)
+    -- PS: size of initrd could be stored right before the begining of the initrd data ?
     note : 0x106010 is the default address when loading an initrd in qemu-system-i386
     and it will be used once a custom bootloader is made
   */
   uint8_t* initrd_begin = (uint8_t*)0x106010;
 
+  char buf[33];
+  itoa(*initrd_begin, buf, 10);
+  printf(buf);
 	__asm__ __volatile__("hlt");
 }
